192badc294feb7ed52b8092d9eda6b02e9d8d105
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-03-08 15:20:48 +0300

    class constuctors changed to uniform with rectangle class

--- lab-1-intro/Objects/Geometry/Star.h
+++ lab-1-intro/Objects/Geometry/Star.h
@@ -6,8 +6,8 @@ class Star : public PrimitiveGeometry
 {
 public:
 	Star();
-	Star(std::vector<Vertex>& v, std::vector<int>& i);
+	Star(const std::vector<Vertex>& v, const std::vector<DWORD>& i);
 
 	virtual ~Star() override;
-
+	virtual void Initialize(ID3D11Device* pDevice, ID3D11DeviceContext* pDeviceContext) override;
 };
