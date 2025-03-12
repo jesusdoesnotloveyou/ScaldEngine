6dcf98f06387ad52dfcbac09ed2b7f3f2b2fb5c5
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-03-12 12:44:05 +0300

    scene changed, the way objects created is changed, added two colliders

--- lab-1-intro/Graphics/ScaldCoreTypes.h
+++ lab-1-intro/Graphics/ScaldCoreTypes.h
@@ -18,6 +18,7 @@ struct ConstBuffer
 struct STransform
 {
 	dx::XMFLOAT3 Rotation = { 0.0f, 0.0f, 0.0f };
-	dx::XMFLOAT3 Translation = { 0.0f, 0.0f, 0.0f };
-	dx::XMFLOAT3 Scale = { 0.0f, 0.0f, 0.0f };
+	dx::XMFLOAT3 Position = { 0.0f, 0.0f, 0.0f };
+	dx::XMFLOAT3 Scale = { 0.0f, 0.0f, 0.0f };		// real scale in coordinate divided by two ([0,x], [0,y], [0, z])
+	dx::XMFLOAT3 Center = { 0.0f, 0.0f, 0.0f };
 };
