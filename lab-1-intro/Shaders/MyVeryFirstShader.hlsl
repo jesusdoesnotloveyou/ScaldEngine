96eca2c522d301cbbe9b01bf4c62d0e60135db87
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-03-16 14:38:25 +0300

    change shader file on two different: Pixel and Vertex, change entry points and setup

--- lab-1-intro/Shaders/MyVeryFirstShader.hlsl
+++ lab-1-intro/Shaders/MyVeryFirstShader.hlsl
@@ -1,5 +1,6 @@
 cbuffer CBuf : register(b0)
 {
+    // matrices are column_major by default
     /*row_major */matrix transform;
 };
 
@@ -10,17 +11,17 @@ cbuffer CBufChangeOnResize : register(b1)
 
 struct VS_IN
 {
-	float4 pos : POSITION0;
-	float4 col : COLOR0;
+    float4 pos : POSITION0;
+    float4 col : COLOR0;
 };
 
 struct PS_IN
 {
-	float4 pos : SV_POSITION;
- 	float4 col : COLOR;
+    float4 pos : SV_POSITION;
+    float4 col : COLOR;
 };
 
-PS_IN VSMain( VS_IN input )
+PS_IN main( VS_IN input )
 {
 	PS_IN output = (PS_IN)0;
 	
@@ -30,11 +31,11 @@ PS_IN VSMain( VS_IN input )
 	return output;
 }
 
-float4 PSMain( PS_IN input ) : SV_Target
-{
-	float4 col = input.col;
-//#ifdef TEST
-//	if (input.pos.x > 400) col = TCOLOR;
-//#endif
-	return col;
-}
+//float4 PSMain( PS_IN input ) : SV_Target
+//{
+//	float4 col = input.col;
+////#ifdef TEST
+////	if (input.pos.x > 400) col = TCOLOR;
+////#endif
+//	return col;
+//}
--- lab-1-intro/Shaders/MyVerySecondShader.hlsl
+++ lab-1-intro/Shaders/MyVerySecondShader.hlsl
@@ -1,14 +1,14 @@
-struct PS_IN
-{
-    float4 pos : SV_POSITION;
-    float4 col : COLOR;
-};
+//struct PS_IN
+//{
+//    float4 pos : SV_POSITION;
+//    float4 col : COLOR;
+//};
 
-float4 PSMain(PS_IN input) : SV_Target
+float4 main(float4 pos : SV_POSITION, float4 col : COLOR) : SV_Target
 {
-    float4 col = input.col;
+    float4 color = col;
 //#ifdef TEST
 //	if (input.pos.x > 400) col = TCOLOR;
 //#endif
-    return col;
+    return color;
 }
