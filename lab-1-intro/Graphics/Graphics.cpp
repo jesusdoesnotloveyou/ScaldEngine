7ac981680054ceeff069ab07ddf6c1f298cd0b8d
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-03-16 14:35:04 +0300

    add camera to Graphics, matrix setup removed from ConstantBuffer since it has been replaced to Camera class

--- lab-1-intro/Graphics/Graphics.cpp
+++ lab-1-intro/Graphics/Graphics.cpp
@@ -16,16 +16,16 @@
 
 Graphics::Graphics(HWND hWnd, int width, int height)
 	:
-	screenWidth(width),
-	screenHeight(height),
+	mScreenWidth(width),
+	mScreenHeight(height),
 	hWnd(hWnd)
 {
 	D3D_FEATURE_LEVEL featureLevel[] = { D3D_FEATURE_LEVEL_11_1 };
 
 	DXGI_SWAP_CHAIN_DESC swapDesc = { };
 	swapDesc.BufferCount = 2;
-	swapDesc.BufferDesc.Width = screenWidth;
-	swapDesc.BufferDesc.Height = screenHeight;
+	swapDesc.BufferDesc.Width = mScreenWidth;
+	swapDesc.BufferDesc.Height = mScreenHeight;
 	swapDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
 	swapDesc.BufferDesc.RefreshRate.Numerator = 60;
 	swapDesc.BufferDesc.RefreshRate.Denominator = 1;
@@ -62,6 +62,20 @@ Graphics::Graphics(HWND hWnd, int width, int height)
 	// check MSDN for more info about GetAddressOf, Get, (&) ReleaseAndGetAddressOf
 	ThrowIfFailed(pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), &pBackTex));
 	ThrowIfFailed(pDevice->CreateRenderTargetView(pBackTex.Get(), nullptr, &pRtv));
+	
+	// Describe our Depth/Stencil Buffer
+	D3D11_TEXTURE2D_DESC depthStencilDesc;
+	depthStencilDesc.Width = mScreenWidth;
+	depthStencilDesc.Height = mScreenHeight;
+	depthStencilDesc.MipLevels = 1u;
+	depthStencilDesc.ArraySize = 1u;
+	depthStencilDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	depthStencilDesc.SampleDesc.Count = 1u;
+	depthStencilDesc.SampleDesc.Quality = 0u;
+	depthStencilDesc.Usage = D3D11_USAGE_DEFAULT;
+	depthStencilDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	depthStencilDesc.CPUAccessFlags = 0u;
+	depthStencilDesc.MiscFlags = 0u;
 }
 
 void Graphics::EndFrame()
@@ -89,6 +103,10 @@ void Graphics::Setup()
 
 	ThrowIfFailed(pDevice->CreateRasterizerState(&rastDesc, &pRastState));
 	pContext->RSSetState(pRastState.Get());
+
+	// Camera setup
+	mCamera.SetPosition(0.0f, 0.0f, -2.0f);
+	mCamera.SetProjectionValues(90.0f, static_cast<float>(mScreenWidth) / mScreenHeight, 0.1f, 1000.0f);
 }
 
 void Graphics::DrawScene(std::vector<PrimitiveGeometry*>& gameObjects)
@@ -101,8 +119,8 @@ void Graphics::DrawScene(std::vector<PrimitiveGeometry*>& gameObjects)
 		D3D11_VIEWPORT {
 			0.0f /*TopLeftX*/,
 			0.0f /*TopLeftY*/,
-			static_cast<float>(screenWidth),
-			static_cast<float>(screenHeight),
+			static_cast<float>(mScreenWidth),
+			static_cast<float>(mScreenHeight),
 			0.0f /*MinDepth*/,
 			1.0f /*MaxDepth*/ },
 	};
@@ -114,7 +132,7 @@ void Graphics::DrawScene(std::vector<PrimitiveGeometry*>& gameObjects)
 		pContext->IASetInputLayout(geometry->GetRenderComponent()->GetInputLayout());
 		pContext->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
-		if (!geometry->GetConstantBuffer().ApplyChanges()) return;
+		if (!geometry->GetConstantBuffer().ApplyChanges(mCamera.GetViewMatrix(), mCamera.GetProjectionMatrix())) return;
 		
 		pContext->VSSetConstantBuffers(0u, 1u, geometry->GetConstantBuffer().GetAddressOf());
 		pContext->IASetVertexBuffers(0u, 1u, geometry->GetVertexBuffer().GetAddressOf(), geometry->GetVertexBuffer().GetStridePtr(), &geometry->offset);
