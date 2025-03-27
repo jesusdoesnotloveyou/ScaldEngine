1ba805bb794141742eae3f34c19ee84dcb1f7c25
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-03-27 12:13:03 +0300

    input&output structures for shaders; making them texture compatible

--- lab-1-intro/Shaders/MyVerySecondShader.hlsl
+++ lab-1-intro/Shaders/MyVerySecondShader.hlsl
@@ -1,14 +1,15 @@
-//struct PS_IN
-//{
-//    float4 pos : SV_POSITION;
-//    float4 col : COLOR;
-//};
-
-float4 main(float4 pos : SV_POSITION, float4 col : COLOR) : SV_Target
+struct PS_IN
 {
-    float4 color = col;
-//#ifdef TEST
-//	if (input.pos.x > 400) col = TCOLOR;
-//#endif
-    return color;
+    float4 inPosition : SV_POSITION;
+    float2 inTexCoord : TEXCOORD;
+};
+
+Texture2D objTexture : TEXTURE : register(t0);
+SamplerState objSamplerState : SAMPLER : register(s0);
+
+float4 main(PS_IN input) : SV_Target
+{
+    float4 pixelColor = objTexture.Sample(objSamplerState, input.inTexCoord);
+
+    return pixelColor;
 }
