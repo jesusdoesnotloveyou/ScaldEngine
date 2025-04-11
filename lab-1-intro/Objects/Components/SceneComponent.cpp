59f2c09c1a0d4c1c6f0e9d6d04f2140788fc6d42
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-04-11 13:37:56 +0300

    base scene component (no visual representation)

---
+++ lab-1-intro/Objects/Components/SceneComponent.cpp
@@ -0,0 +1,93 @@
+#include "SceneComponent.h"
+#include "TransformComponent.h"
+
+SceneComponent::SceneComponent()
+{
+	mTransformComponent = new TransformComponent();
+}
+
+SceneComponent::~SceneComponent()
+{
+	if (mTransformComponent) delete mTransformComponent;
+}
+
+void SceneComponent::Update(const ScaldTimer& st)
+{
+	//ScaldComponent::Update(st);
+	mTransformComponent->Update(st);
+}
+
+void SceneComponent::SetPosition(const XMVECTOR& pos)
+{
+	mTransformComponent->SetPosition(pos);
+}
+
+void SceneComponent::SetPosition(float x, float y, float z)
+{
+	mTransformComponent->SetPosition(x, y, z);
+}
+
+void SceneComponent::AdjustPosition(const XMVECTOR& pos)
+{
+	mTransformComponent->AdjustPosition(pos);
+}
+
+void SceneComponent::AdjustPosition(float x, float y, float z)
+{
+	mTransformComponent->AdjustPosition(x, y, z);
+}
+
+void SceneComponent::SetRotation(const XMVECTOR& rot)
+{
+	mTransformComponent->SetRotation(rot);
+}
+
+void SceneComponent::SetRotation(float x, float y, float z)
+{
+	mTransformComponent->SetRotation(x, y, z);
+}
+
+void SceneComponent::AdjustRotation(const XMVECTOR& rot)
+{
+	mTransformComponent->AdjustRotation(rot);
+}
+
+void SceneComponent::AdjustRotation(float x, float y, float z)
+{
+	mTransformComponent->AdjustRotation(x, y, z);
+}
+
+void SceneComponent::SetScale(const XMVECTOR& scale)
+{
+	mTransformComponent->SetScale(scale);
+}
+
+void SceneComponent::SetScale(float x, float y, float z)
+{
+	mTransformComponent->SetScale(x, y, z);
+}
+
+void SceneComponent::AdjustScale(const XMVECTOR& scale)
+{
+	mTransformComponent->AdjustScale(scale);
+}
+
+void SceneComponent::AdjustScale(float x, float y, float z)
+{
+	mTransformComponent->AdjustScale(x, y, z);
+}
+
+void SceneComponent::SetForwardVector(const XMVECTOR& relativeForwardVector)
+{
+	mTransformComponent->SetForwardVector(relativeForwardVector);
+}
+
+void SceneComponent::SetRightVector(const XMVECTOR& relativeRightVector)
+{
+	mTransformComponent->SetRightVector(relativeRightVector);
+}
+
+void SceneComponent::SetUpVector(const XMVECTOR& relativeUpVector)
+{
+	mTransformComponent->SetUpVector(relativeUpVector);
+}
