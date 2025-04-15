f3e19a52b2db8bf1f6cf730d51fca95b2d1540c7
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-04-15 13:22:20 +0300

    normal applying result in pixel shader

--- lab-1-intro/Shaders/MyVerySecondShader.hlsl
+++ lab-1-intro/Shaders/MyVerySecondShader.hlsl
@@ -21,8 +21,8 @@ SamplerState objSamplerState : SAMPLER : register(s0);
 
 float4 main(PS_IN input) : SV_Target
 {
-    float4 sampleColor = objTexture.Sample(objSamplerState, input.inTexCoord);
-
+    //float4 sampleColor = objTexture.Sample(objSamplerState, input.inTexCoord);
+    float3 sampleColor = input.inNormal;
     float3 ambientLight = ambientLightColor * ambientLightStrength;
     float3 finalColor = sampleColor.xyz * ambientLight;
     
