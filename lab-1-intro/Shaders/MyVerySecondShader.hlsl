ff0a62a230d5c663f1d4e6486ade8a468de57f8a
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-04-15 16:52:14 +0300

    light calculations; constBuffer updated; worldPositiona added to PS_IN

--- lab-1-intro/Shaders/MyVerySecondShader.hlsl
+++ lab-1-intro/Shaders/MyVerySecondShader.hlsl
@@ -7,6 +7,10 @@ cbuffer lightBuffer : register(b0)
 {
     float3 ambientLightColor;
     float ambientLightStrength;
+    
+    float3 dynamicLightColor;
+    float dynamicLightStrength;
+    float3 dynamicLightPosition;
 };
 
 struct PS_IN
@@ -14,6 +18,7 @@ struct PS_IN
     float4 inPosition : SV_POSITION;
     float2 inTexCoord : TEXCOORD;
     float3 inNormal : NORMAL;
+    float3 inWorldPos: WORLD_POSITION;
 };
 
 Texture2D objTexture : TEXTURE : register(t0);
@@ -21,10 +26,22 @@ SamplerState objSamplerState : SAMPLER : register(s0);
 
 float4 main(PS_IN input) : SV_Target
 {
-    //float4 sampleColor = objTexture.Sample(objSamplerState, input.inTexCoord);
-    float3 sampleColor = input.inNormal;
+    float4 sampleColor = objTexture.Sample(objSamplerState, input.inTexCoord);
+    //float3 sampleColor = input.inNormal;
     float3 ambientLight = ambientLightColor * ambientLightStrength;
-    float3 finalColor = sampleColor.xyz * ambientLight;
+    
+    float3 appliedLight = ambientLight;
+    
+    float3 lightVector = normalize(dynamicLightPosition - input.inWorldPos);
+    
+    float3 diffuseLightIntensity = max(dot(lightVector, input.inNormal), 0.0f);
+    // for testing: more explicit view of light
+    //float3 diffuseLightIntensity = (dot(lightVector, input.inNormal));
+    float3 diffuseLight = diffuseLightIntensity * dynamicLightStrength * dynamicLightColor;
+    
+    appliedLight += diffuseLight;
+    
+    float3 finalColor = sampleColor.xyz * appliedLight;
     
     return float4(finalColor.xyz, 1.0f);
 }
