de8a401a250758b1bb165badbc899b9ee9b46adb
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-04-19 19:55:50 +0300

    IMPORTANT: Refactor of Phong Model of Lighting, first attempt to implement specular light

--- lab-1-intro/Shaders/MyVerySecondShader.hlsl
+++ lab-1-intro/Shaders/MyVerySecondShader.hlsl
@@ -17,6 +17,13 @@ cbuffer lightBuffer : register(b0)
     float attenuationExponentialFactor;
 };
 
+cbuffer cbPerFrame : register(b1)
+{
+    float4 gEyePos;
+    //float Ks = 0.1f;
+    //float shininess = 200;
+};
+
 struct PS_IN
 {
     float4 inPosition : SV_POSITION;
@@ -28,26 +35,28 @@ struct PS_IN
 Texture2D objTexture : TEXTURE : register(t0);
 SamplerState objSamplerState : SAMPLER : register(s0);
 
+
 float4 main(PS_IN input) : SV_Target
 {
+    float3 lightVector = normalize(dynamicLightPosition - input.inWorldPos);
+    float3 reflectLight = normalize(reflect(lightVector, input.inNormal));
     float4 sampleColor = objTexture.Sample(objSamplerState, input.inTexCoord);
+    float3 viewDir = normalize(gEyePos.xyz - input.inWorldPos);
+    
     //float3 sampleColor = input.inNormal;
     float3 ambientLight = ambientLightColor * ambientLightStrength;
-    float3 appliedLight = ambientLight;
-    float3 lightVector = normalize(dynamicLightPosition - input.inWorldPos);
     
-    float3 diffuseLightIntensity = /*saturate*/(max(dot(lightVector, input.inNormal), 0.0f));
-    // for testing: more explicit view of light
-    //float3 diffuseLightIntensity = (dot(lightVector, input.inNormal));
-    
-    // distance to pixel from light source
-    float distanceToLight = distance(dynamicLightPosition, input.inWorldPos);
-    
-    float attenuationFactor = pow(attenuationConstantFactor + attenuationLinearFactor * distanceToLight + attenuationExponentialFactor * pow(distanceToLight, 2), -1);
-    diffuseLightIntensity *= attenuationFactor;
-    
+    //float3 diffuseLightIntensity = (dot(lightVector, input.inNormal)); // for testing: more explicit view of light propagation
+    float3 diffuseLightIntensity = saturate(max(dot(lightVector, input.inNormal), 0.0f));
     float3 diffuseLight = diffuseLightIntensity * dynamicLightStrength * dynamicLightColor;
-    appliedLight += diffuseLight;
+    
+    float3 specularIntensity = 10.0f * pow(max(dot(reflectLight, viewDir), 0.0f), 90.0f);
+    float3 specularLight = saturate(specularIntensity);
+    
+    float distanceToLight = distance(dynamicLightPosition, input.inWorldPos);
+    float attenuationFactor = pow(attenuationConstantFactor + attenuationLinearFactor * distanceToLight + attenuationExponentialFactor * pow(distanceToLight, 2), -1);
+    
+    float3 appliedLight = ambientLight + (diffuseLight + specularLight) * attenuationFactor;
     float3 finalColor = sampleColor.xyz * appliedLight;
     
     return float4(finalColor.xyz, 1.0f);
