86f92db97995c75f3f775e820cce442b2b6c7331
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-04-21 00:08:20 +0300

    pixel shader for shadows

---
+++ lab-1-intro/Shaders/MyVeryThirdShader(Shadows).hlsl
@@ -0,0 +1,3 @@
+Texture2D shaderTexture : register(t0);
+// The depthMapTexture is the shadow map. This texture contains the scene depth buffer rendered from the light's perspective.
+Texture2D depthMapTexture : register(t1);
