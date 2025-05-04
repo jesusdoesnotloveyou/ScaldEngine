6311ff0ecd7975ed42ace329b8ce2c2b49a4a09a
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-05-04 11:54:50 +0300

    IMPORTANT: Structured buffer for light setup

--- lab-1-intro/Graphics/Graphics.h
+++ lab-1-intro/Graphics/Graphics.h
@@ -9,7 +9,7 @@
 
 #include "Shaders.h"
 #include "ConstantBuffer.h"
-#include "ScaldCoreTypes.h"
+#include "Light/LightHelper.h"
 
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "d3dcompiler.lib")
@@ -31,7 +31,8 @@ public:
 	
 	void Setup();
 	void InitSceneObjects(std::vector<SceneGeometry*>& sceneObjects);
-	void AddLightSource(Light* lightObject);
+	void AddLightSourceParams(PointLight* lightParams);
+	void UpdateLightParams(SceneGeometry* light);
 
 	void ClearBuffer(float r);
 	void DrawScene(std::vector<SceneGeometry*>& sceneObjects);
@@ -40,11 +41,44 @@ public:
 	FORCEINLINE ThirdPersonCamera* GetCamera() const { return mTPCamera; }
 private:
 	void SetupShaders();
+	void SetupLight();
+
+	template<typename T>
+	bool ApplyChanges(ID3D11DeviceContext* deviceContext, ID3D11Buffer* buffer, const std::vector<T>& bufferData)
+	{
+		D3D11_MAPPED_SUBRESOURCE mappedResource;
+		ThrowIfFailed(deviceContext->Map(buffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource));
+
+		CopyMemory(mappedResource.pData, bufferData.data(), sizeof(T) * bufferData.size());
+		deviceContext->Unmap(buffer, 0);
+		return true;
+	}
+
+	template<typename T>
+	HRESULT CreateStructuredBuffer(ID3D11Device* device, ID3D11Buffer** buffer, const std::vector<T>& bufferData)
+	{
+		UINT stride = (UINT)sizeof(T);
+		UINT byteWidth = stride * (UINT)bufferData.size();
+
+		D3D11_BUFFER_DESC desc;
+		desc.ByteWidth = byteWidth;
+		desc.Usage = D3D11_USAGE_DYNAMIC;
+		desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+		desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+		desc.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
+		desc.StructureByteStride = stride;
+
+		D3D11_SUBRESOURCE_DATA data;
+		data.pSysMem = bufferData.data();
+		data.SysMemPitch = 0;
+		data.SysMemSlicePitch = 0;
+
+		return device->CreateBuffer(&desc, &data, buffer);
+	}
 
 private:
 	Camera* mCamera = nullptr;
 	ThirdPersonCamera* mTPCamera = nullptr;
-	std::vector<Light*> mLights;
 
 	int mScreenWidth;
 	int mScreenHeight;
@@ -53,7 +87,15 @@ private:
 	VertexShader mVertexShader;
 	PixelShader mPixelShader;
 
+#pragma region Light
 	ConstantBuffer<ConstBufferPerFrame> mCB;
+
+	// need to update members of vector
+	std::vector<PointLight> mLightsParameters;
+
+	Microsoft::WRL::ComPtr<ID3D11Buffer> mLightBuffer;
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> mLightShaderResourceView;
+#pragma endregion Light
 
 	Microsoft::WRL::ComPtr<ID3D11Device> mDevice;
 	Microsoft::WRL::ComPtr<IDXGISwapChain> mSwapChain;
