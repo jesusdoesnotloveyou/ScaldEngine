15f2ad48cdd483cfb7c1bb2e906c8c30fc949eb1
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-05-04 12:01:25 +0300

    for future shadows

--- lab-1-intro/Graphics/Shadows/ShadowMap.cpp
+++ lab-1-intro/Graphics/Shadows/ShadowMap.cpp
@@ -3,7 +3,11 @@
 
 ShadowMap::ShadowMap(ID3D11Device* device, UINT width, UINT height)
 	:
-	mWidth(width), mHeight(height), mDepthMapSRV(nullptr), mDepthMapDSV(nullptr)
+	mWidth(width),
+	mHeight(height),
+	mShadowMap(nullptr),
+	mDepthMapSRV(nullptr),
+	mDepthMapDSV(nullptr)
 {
 	// Viewport that matches the shadow map dimensions.
 	mViewport.TopLeftX = 0.0f;
@@ -15,48 +19,83 @@ ShadowMap::ShadowMap(ID3D11Device* device, UINT width, UINT height)
 	// Use typeless format because the DSV is going to interpret
 	// the bits as DXGI_FORMAT_D24_UNORM_S8_UINT, whereas the SRV is going
 	// to interpret the bits as DXGI_FORMAT_R24_UNORM_X8_TYPELESS.
-	D3D11_TEXTURE2D_DESC texDesc;
-	texDesc.Width = mWidth;
-	texDesc.Height = mHeight;
-	texDesc.MipLevels = 1;
-	texDesc.ArraySize = 1;
-	texDesc.Format = DXGI_FORMAT_R24G8_TYPELESS;
-	texDesc.SampleDesc.Count = 1;
-	texDesc.SampleDesc.Quality = 0;
-	texDesc.Usage = D3D11_USAGE_DEFAULT;
-	texDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL | D3D11_BIND_SHADER_RESOURCE;
-	texDesc.CPUAccessFlags = 0;
-	texDesc.MiscFlags = 0;
+	D3D11_TEXTURE2D_DESC shadowMapDesc;
+	shadowMapDesc.Width = mWidth;
+	shadowMapDesc.Height = mHeight;
+	shadowMapDesc.MipLevels = 1u;
+	shadowMapDesc.ArraySize = 1u;
+	shadowMapDesc.Format = DXGI_FORMAT_R24G8_TYPELESS;
+	shadowMapDesc.SampleDesc.Count = 1u;
+	shadowMapDesc.SampleDesc.Quality = 0;
+	shadowMapDesc.Usage = D3D11_USAGE_DEFAULT;
+	shadowMapDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL | D3D11_BIND_SHADER_RESOURCE;
+	shadowMapDesc.CPUAccessFlags = 0u;
+	shadowMapDesc.MiscFlags = 0u;
 
-	ID3D11Texture2D* depthMap = 0;
-	ThrowIfFailed(device->CreateTexture2D(&texDesc, 0, &depthMap));
+	ThrowIfFailed(device->CreateTexture2D(&shadowMapDesc, nullptr, &mShadowMap));
 
-	D3D11_DEPTH_STENCIL_VIEW_DESC dsvDesc;
-	dsvDesc.Flags = 0;
-	dsvDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
-	dsvDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
-	dsvDesc.Texture2D.MipSlice = 0;
-	ThrowIfFailed(device->CreateDepthStencilView(depthMap, &dsvDesc, &mDepthMapDSV));
+	D3D11_DEPTH_STENCIL_VIEW_DESC depthStencilViewDesc;
+	ZeroMemory(&depthStencilViewDesc, sizeof(D3D11_DEPTH_STENCIL_VIEW_DESC));
+	depthStencilViewDesc.Flags = 0u;
+	depthStencilViewDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	depthStencilViewDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+	depthStencilViewDesc.Texture2D.MipSlice = 0u;
 
-	D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc;
-	srvDesc.Format = DXGI_FORMAT_R24_UNORM_X8_TYPELESS;
-	srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
-	srvDesc.Texture2D.MipLevels = texDesc.MipLevels;
-	srvDesc.Texture2D.MostDetailedMip = 0;
-	ThrowIfFailed(device->CreateShaderResourceView(depthMap, &srvDesc, &mDepthMapSRV));
+	D3D11_SHADER_RESOURCE_VIEW_DESC shaderResourceViewDesc;
+	shaderResourceViewDesc.Format = DXGI_FORMAT_R24_UNORM_X8_TYPELESS;
+	shaderResourceViewDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+	shaderResourceViewDesc.Texture2D.MipLevels = shadowMapDesc.MipLevels;
+	shaderResourceViewDesc.Texture2D.MostDetailedMip = 0u;
+
+	ThrowIfFailed(device->CreateDepthStencilView(mShadowMap, &depthStencilViewDesc, &mDepthMapDSV));
+	ThrowIfFailed(device->CreateShaderResourceView(mShadowMap, &shaderResourceViewDesc, &mDepthMapSRV));
 	
-	// View saves a reference to the texture so we can release our
-	// reference.
+	// View saves a reference to the texture so we can release our reference.
 	//ReleaseCOM(depthMap);
 	
+	D3D11_SAMPLER_DESC comparisonSamplerDesc;
+	ZeroMemory(&comparisonSamplerDesc, sizeof(D3D11_SAMPLER_DESC));
+	comparisonSamplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_BORDER;
+	comparisonSamplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_BORDER;
+	comparisonSamplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_BORDER;
+	comparisonSamplerDesc.BorderColor[0] = 1.0f;
+	comparisonSamplerDesc.BorderColor[1] = 1.0f;
+	comparisonSamplerDesc.BorderColor[2] = 1.0f;
+	comparisonSamplerDesc.BorderColor[3] = 1.0f;
+	comparisonSamplerDesc.MinLOD = 0.f;
+	comparisonSamplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
+	comparisonSamplerDesc.MipLODBias = 0.f;
+	comparisonSamplerDesc.MaxAnisotropy = 0;
+	comparisonSamplerDesc.ComparisonFunc = D3D11_COMPARISON_LESS_EQUAL;
+	comparisonSamplerDesc.Filter = D3D11_FILTER_COMPARISON_MIN_MAG_MIP_POINT;
+
+	// Point filtered shadows can be faster, and may be a good choice when
+	// rendering on hardware with lower feature levels. This sample has a
+	// UI option to enable/disable filtering so you can see the difference
+	// in quality and speed.
+	ThrowIfFailed(device->CreateSamplerState(&comparisonSamplerDesc, &mSamplerState));
+
 	D3D11_RASTERIZER_DESC rastDesc;
+	ZeroMemory(&rastDesc, sizeof(D3D11_RASTERIZER_DESC));
+	rastDesc.CullMode = D3D11_CULL_BACK;
+	rastDesc.FillMode = D3D11_FILL_SOLID;
 	rastDesc.DepthBias = 100000;
 	rastDesc.DepthBiasClamp = 0.0f;
 	rastDesc.SlopeScaledDepthBias = 1.0f;
+	rastDesc.DepthClipEnable = true; // Feature level 9_1 requires DepthClipEnable == true
+	ThrowIfFailed(device->CreateRasterizerState(&rastDesc, &mRasterizerState));
+	
+	D3D11_RASTERIZER_DESC shadowRenderStateDesc;
+	ZeroMemory(&shadowRenderStateDesc, sizeof(D3D11_RASTERIZER_DESC));
+	shadowRenderStateDesc.CullMode = D3D11_CULL_FRONT;
+	shadowRenderStateDesc.FillMode = D3D11_FILL_SOLID;
+	shadowRenderStateDesc.DepthClipEnable = true;
+	ThrowIfFailed(device->CreateRasterizerState(&shadowRenderStateDesc, &mShadowRastState));
 }
 
 ShadowMap::~ShadowMap() noexcept
 {
+
 }
 
 ID3D11ShaderResourceView* ShadowMap::DepthMapSRV()
@@ -73,4 +112,8 @@ void ShadowMap::BindDsvAndSetNullRenderTarget(ID3D11DeviceContext* dc)
 	ID3D11RenderTargetView* renderTargets[1] = { 0 };
 	dc->OMSetRenderTargets(1u, renderTargets, mDepthMapDSV);
 	dc->ClearDepthStencilView(mDepthMapDSV, D3D11_CLEAR_DEPTH, 1.0f, 0u);
+}
+
+void ShadowMap::Setup()
+{
 }
