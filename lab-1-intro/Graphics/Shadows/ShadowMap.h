47ca66ce76a5315baaf3df6b04650b23f7be235b
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-05-05 19:32:06 +0300

    remove some redundant and useless code; refactor; memory free

--- lab-1-intro/Graphics/Shadows/ShadowMap.h
+++ lab-1-intro/Graphics/Shadows/ShadowMap.h
@@ -7,26 +7,23 @@ class ShadowMap
 public:
 	ShadowMap(ID3D11Device* device, UINT width, UINT height);
 	~ShadowMap() noexcept;
-	ID3D11ShaderResourceView* DepthMapSRV();
+	ID3D11ShaderResourceView* GetDepthMapSRV();
 	void BindDsvAndSetNullRenderTarget(ID3D11DeviceContext* dc);
-
-	void Setup();
 private:
 	ShadowMap(const ShadowMap& lhs);
 	ShadowMap& operator=(const ShadowMap& lhs) = default;
 
 private:
 	// square 512, 1024, 2048, 4096 
-	UINT mWidth;
-	UINT mHeight;
-	
-	ID3D11Texture2D* mShadowMap;
+	UINT mWidth = 2048;
+	UINT mHeight = 2048;
+
 	ID3D11ShaderResourceView* mDepthMapSRV;
 	ID3D11DepthStencilView* mDepthMapDSV;
 	D3D11_VIEWPORT mViewport;
 	
-	ID3D11SamplerState* mSamplerState;
+	/*ID3D11SamplerState* mSamplerState;
 
 	ID3D11RasterizerState* mRasterizerState;
-	ID3D11RasterizerState* mShadowRastState;
+	ID3D11RasterizerState* mShadowRastState;*/
 };
