0f0461774f8e610888cb64458b6e787ac55eb98d
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-05-05 17:49:02 +0300

    vertex shader for shadows pretty much completed

--- lab-1-intro/Shaders/ShadowVertexShader.hlsl
+++ lab-1-intro/Shaders/ShadowVertexShader.hlsl
@@ -1,11 +1,3 @@
-
-// MOVE TO CORE PIXEL SHADER //////////
-
-Texture2D shaderTexture : register(t0);
-//The depthMapTexture is the shadow map. This texture contains the scene depth buffer rendered from the light's perspective.
-Texture2D depthMapTexture : register(t1);
-///////////////////////////////////////
-
 cbuffer cbPerObject : register(b0)
 {
     matrix WVP;
