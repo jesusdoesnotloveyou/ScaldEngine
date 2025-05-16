b11987561d4c38af073ca937c05bd42d54e2e59d
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-05-16 17:04:59 +0300

    runtime shader error fixed: changed sampler filer -> for shadow we use COMPARISON

--- lab-1-intro/Graphics/Graphics.cpp
+++ lab-1-intro/Graphics/Graphics.cpp
@@ -398,7 +398,7 @@ void Graphics::CreateSamplerState()
 
 	D3D11_SAMPLER_DESC shadowSampDesc = {};
 	ZeroMemory(&shadowSampDesc, sizeof(shadowSampDesc));
-	shadowSampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	shadowSampDesc.Filter = D3D11_FILTER_COMPARISON_MIN_MAG_LINEAR_MIP_POINT;
 	shadowSampDesc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
 	shadowSampDesc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
 	shadowSampDesc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
