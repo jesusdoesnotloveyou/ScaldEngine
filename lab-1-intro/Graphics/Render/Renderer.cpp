52c78c1f12c4f43704d50d3aec5e53c2d3f625f2
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-05-22 14:55:09 +0300

    common functionality added to render interface

--- lab-1-intro/Graphics/Render/Renderer.cpp
+++ lab-1-intro/Graphics/Render/Renderer.cpp
@@ -1,8 +1,91 @@
 #include "Renderer.h"
 
-Renderer::Renderer(ID3D11Device* device, ID3D11DeviceContext* deviceContext, UINT width, UINT height)
+Renderer::Renderer(ID3D11Device* device, ID3D11DeviceContext* deviceContext, int width, int height)
 	:
-	mDepthStencilView(nullptr)
+	mDevice(device),
+	mDeviceContext(deviceContext),
+	mDepthStencilView(nullptr),
+	mScreenWidth(width),
+	mScreenHeight(height)
 {
 
+}
+
+void Renderer::SetupShaders()
+{
+	D3D11_INPUT_ELEMENT_DESC inputLayoutShadowDesc[] = {
+		D3D11_INPUT_ELEMENT_DESC {
+			"POSITION",
+			0u,
+			DXGI_FORMAT_R32G32B32A32_FLOAT,
+			0u,
+			0u,
+			D3D11_INPUT_PER_VERTEX_DATA,
+			0u}
+	};
+
+	ThrowIfFailed(mShadowVertexShader.Init(mDevice, inputLayoutShadowDesc, (UINT)std::size(inputLayoutShadowDesc), L"./Shaders/ShadowVertexShader.hlsl"));
+	ThrowIfFailed(mCSMGeometryShader.Init(mDevice, L"./Shaders/CSMGeometryShader.hlsl"));
+}
+
+void Renderer::CreateDepthStencilState()
+{
+	//Create depth stencil state
+	CD3D11_DEPTH_STENCIL_DESC depthStencilDesc;
+	ZeroMemory(&depthStencilDesc, sizeof(CD3D11_DEPTH_STENCIL_DESC));
+	depthStencilDesc.DepthEnable = true;
+	depthStencilDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK::D3D11_DEPTH_WRITE_MASK_ALL;
+	depthStencilDesc.DepthFunc = D3D11_COMPARISON_FUNC::D3D11_COMPARISON_LESS_EQUAL;
+	ThrowIfFailed(mDevice->CreateDepthStencilState(&depthStencilDesc, mDepthStencilState.GetAddressOf()));
+}
+
+void Renderer::CreateRasterizerState()
+{
+	// Step 10: Setup Rasterizer Stage and Viewport
+	mViewport.TopLeftX = 0.0f;
+	mViewport.TopLeftY = 0.0f;
+	mViewport.Width = static_cast<float>(mScreenWidth);
+	mViewport.Height = static_cast<float>(mScreenHeight);
+	mViewport.MinDepth = 0.0f;
+	mViewport.MaxDepth = 1.0f;
+
+	CD3D11_RASTERIZER_DESC rastDesc = {};
+	rastDesc.FillMode = D3D11_FILL_SOLID;
+	rastDesc.CullMode = D3D11_CULL_BACK;
+	rastDesc.FrontCounterClockwise = false;
+	rastDesc.DepthBias;
+	rastDesc.DepthBiasClamp;
+	rastDesc.SlopeScaledDepthBias;
+	ThrowIfFailed(mDevice->CreateRasterizerState(&rastDesc, mRasterizerState.GetAddressOf()));
+}
+
+void Renderer::CreateSamplerState()
+{
+	D3D11_SAMPLER_DESC sampDesc = {};
+	ZeroMemory(&sampDesc, sizeof(sampDesc));
+	sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
+	sampDesc.MinLOD = 0;
+	sampDesc.MaxLOD = D3D11_FLOAT32_MAX;
+	ThrowIfFailed(mDevice->CreateSamplerState(&sampDesc, mSamplerState.GetAddressOf()));
+
+	D3D11_SAMPLER_DESC shadowSampDesc = {};
+	ZeroMemory(&shadowSampDesc, sizeof(shadowSampDesc));
+	shadowSampDesc.Filter = D3D11_FILTER_COMPARISON_MIN_MAG_LINEAR_MIP_POINT;
+	shadowSampDesc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
+	shadowSampDesc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
+	shadowSampDesc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
+	shadowSampDesc.MipLODBias = 0.0f;
+	shadowSampDesc.MaxAnisotropy = 1u;
+	shadowSampDesc.ComparisonFunc = D3D11_COMPARISON_LESS_EQUAL;
+	shadowSampDesc.BorderColor[0] = 1.0f;
+	shadowSampDesc.BorderColor[1] = 1.0f;
+	shadowSampDesc.BorderColor[2] = 1.0f;
+	shadowSampDesc.BorderColor[3] = 1.0f;
+	shadowSampDesc.MinLOD = 0.0f;
+	shadowSampDesc.MaxLOD = D3D11_FLOAT32_MAX;
+	ThrowIfFailed(mDevice->CreateSamplerState(&shadowSampDesc, mShadowSamplerState.GetAddressOf()));
 }
