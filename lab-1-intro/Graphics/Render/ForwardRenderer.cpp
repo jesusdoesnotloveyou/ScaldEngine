93af88ec250f2c0934baeba78b7e2ed6b2b5b9b4
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-05-22 14:56:14 +0300

    some functions from graphics for forward rendering was moved here

--- lab-1-intro/Graphics/Render/ForwardRenderer.cpp
+++ lab-1-intro/Graphics/Render/ForwardRenderer.cpp
@@ -4,3 +4,38 @@ ForwardRenderer::ForwardRenderer(ID3D11Device* device, ID3D11DeviceContext* devi
 	: Renderer(device, deviceContext, width, height)
 {
 }
+
+void ForwardRenderer::SetupShaders()
+{
+	Renderer::SetupShaders();
+
+	D3D11_INPUT_ELEMENT_DESC inputLayoutDefaultDesc[] = {
+		D3D11_INPUT_ELEMENT_DESC {
+			"POSITION",
+			0u,
+			DXGI_FORMAT_R32G32B32A32_FLOAT,
+			0u,
+			0u,
+			D3D11_INPUT_PER_VERTEX_DATA,
+			0u},
+		D3D11_INPUT_ELEMENT_DESC {
+			"TEXCOORD",
+			0u,
+			DXGI_FORMAT_R32G32_FLOAT,
+			0u,
+			D3D11_APPEND_ALIGNED_ELEMENT,
+			D3D11_INPUT_PER_VERTEX_DATA,
+			0u},
+		D3D11_INPUT_ELEMENT_DESC {
+			"NORMAL",
+			0u,
+			DXGI_FORMAT_R32G32B32_FLOAT,
+			0u,
+			D3D11_APPEND_ALIGNED_ELEMENT,
+			D3D11_INPUT_PER_VERTEX_DATA,
+			0u}
+	};
+
+	ThrowIfFailed(mVertexShader.Init(mDevice, inputLayoutDefaultDesc, (UINT)std::size(inputLayoutDefaultDesc), L"./Shaders/VertexShader.hlsl"));
+	ThrowIfFailed(mPixelShader.Init(mDevice, L"./Shaders/PixelShader.hlsl"));
+}
