6fa0219096882aa688269d7910377878e783ecdb
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-05-23 21:23:36 +0300

    lighting pass pixel shader fix -> final image is rendered properly now

--- lab-1-intro/Shaders/DeferredLightingPS.hlsl
+++ lab-1-intro/Shaders/DeferredLightingPS.hlsl
@@ -1,8 +1,6 @@
-Texture2D<float4> unusedTexture : register(t0);
-
-Texture2D<float4> diffuseTexture : register(t1);
-Texture2D<float3> normalTexture : register(t2);
-Texture2D<float3> posTexture : register(t3);
+Texture2D<float4> diffuseTexture : register(t0);
+Texture2D<float3> normalTexture : register(t1);
+Texture2D<float3> posTexture : register(t2);
 
 SamplerComparisonState shadowSamplerState : SAMPLER : register(s0);
 
@@ -15,7 +13,12 @@ struct PS_IN
 float4 main(PS_IN input) : SV_Target
 {
     float4 diffuseColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
-    diffuseTexture.Load(diffuseColor.xyz);
+    float3 normal = float3(0.0f, 0.0f, 0.0f);
+    float3 pos = float3(0.0f, 0.0f, 0.0f);
+    
+    diffuseColor = diffuseTexture.Load(input.inPosition.xyz);
+    normal = normalTexture.Load(input.inPosition.xyz);
+    pos = posTexture.Load(input.inPosition.xyz);
     
     //output.LightAccumulation = (ambient + emissive);
     //output.Diffuse = diffuse;
