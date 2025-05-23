5506cdd50321ab0439f645b97ef5b40d937725ec
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-05-23 18:14:04 +0300

    deferred renderer initial light stage setup

--- lab-1-intro/Graphics/Render/DeferredRenderer.cpp
+++ lab-1-intro/Graphics/Render/DeferredRenderer.cpp
@@ -2,11 +2,10 @@
 #include "DeferredRenderer.h"
 #include "../ScaldCoreTypes.h"
 
-DeferredRenderer::DeferredRenderer(ID3D11Device* device, ID3D11DeviceContext* deviceContext, UINT width, UINT height)
-	: Renderer(device, deviceContext, width, height)
+DeferredRenderer::DeferredRenderer(IDXGISwapChain* spawChain, ID3D11Device* device, ID3D11DeviceContext* deviceContext, UINT width, UINT height)
+	: Renderer(spawChain, device, deviceContext, width, height)
 {
-	// See Step 03 in Graphics -> same logic, but in that case a number of rtv
-
+	// See Step 03 in Graphics -> same logic, but in this case there is a number of rtvs
 	D3D11_TEXTURE2D_DESC textureDesc = {};
 	ZeroMemory(&textureDesc, sizeof(D3D11_TEXTURE2D_DESC));
 	textureDesc.Width = width;
@@ -48,37 +47,10 @@ DeferredRenderer::DeferredRenderer(ID3D11Device* device, ID3D11DeviceContext* de
 	{
 		ThrowIfFailed(device->CreateShaderResourceView(mGBuffer[i].texture, &shaderResourceViewDesc, &mGBuffer[i].srv));
 	}
-
-	ID3D11Texture2D* depthStencilBuffer = nullptr;
-	D3D11_TEXTURE2D_DESC depthStencilTextureDesc = {};
-	ZeroMemory(&textureDesc, sizeof(D3D11_TEXTURE2D_DESC));
-	depthStencilTextureDesc.Width = width;
-	depthStencilTextureDesc.Height = height;
-	depthStencilTextureDesc.MipLevels = 1u;
-	depthStencilTextureDesc.ArraySize = 1u;
-	depthStencilTextureDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
-	depthStencilTextureDesc.SampleDesc.Count = 1u;
-	depthStencilTextureDesc.SampleDesc.Quality = 0u;
-	depthStencilTextureDesc.Usage = D3D11_USAGE_DEFAULT;
-	depthStencilTextureDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
-	depthStencilTextureDesc.CPUAccessFlags = 0u;
-	depthStencilTextureDesc.MiscFlags = 0u;
-	ThrowIfFailed(device->CreateTexture2D(&depthStencilTextureDesc, nullptr, &depthStencilBuffer));
-
-	D3D11_DEPTH_STENCIL_VIEW_DESC depthStencilViewDesc = {};
-	ZeroMemory(&depthStencilViewDesc, sizeof(D3D11_DEPTH_STENCIL_VIEW_DESC));
-	depthStencilViewDesc.Format = depthStencilTextureDesc.Format;
-	depthStencilViewDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
-	depthStencilViewDesc.Flags = 0u;
-	ThrowIfFailed(device->CreateDepthStencilView(depthStencilBuffer, &depthStencilViewDesc, &mDepthStencilView));
-
-	depthStencilBuffer->Release();
 }
 
 DeferredRenderer::~DeferredRenderer() noexcept
 {
-	mDepthStencilView->Release();
-
 	for (UINT i = 0; i < BUFFER_COUNT; i++)
 	{
 		mGBuffer[i].texture->Release();
@@ -118,14 +90,36 @@ void DeferredRenderer::SetupShaders()
 		0u}
 	};
 
+	D3D11_INPUT_ELEMENT_DESC inputLayoutLightingDesc[] = {
+	D3D11_INPUT_ELEMENT_DESC {
+		"POSITION",
+		0u,
+		DXGI_FORMAT_R32G32B32A32_FLOAT,
+		0u,
+		0u,
+		D3D11_INPUT_PER_VERTEX_DATA,
+		0u},
+	D3D11_INPUT_ELEMENT_DESC {
+		"TEXCOORD",
+		0u,
+		DXGI_FORMAT_R32G32_FLOAT,
+		0u,
+		D3D11_APPEND_ALIGNED_ELEMENT,
+		D3D11_INPUT_PER_VERTEX_DATA,
+		0u}
+	};
+
 	mOpaqueVertexShader.Init(mDevice, inputLayoutOpaqueDesc, (UINT)std::size(inputLayoutOpaqueDesc), L"./Shaders/DeferredOpaqueVS.hlsl");
 	mOpaquePixelShader.Init(mDevice, L"./Shaders/DeferredOpaquePS.hlsl");
+
+	mLightingVertexShader.Init(mDevice, inputLayoutLightingDesc, (UINT)std::size(inputLayoutLightingDesc), L"./Shaders/DeferredLightingVS.hlsl");
+	mLightingPixelShader.Init(mDevice, L"./Shaders/DeferredLightingPS.hlsl");
 }
 
 void DeferredRenderer::BindGeometryPass()
 {
 	mDeviceContext->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-	mDeviceContext->RSSetViewports(1u, &mViewport);
+	mDeviceContext->IASetInputLayout(mOpaqueVertexShader.GetInputLayout());
 
 	ID3D11RenderTargetView* rtvs[] =
 	{
@@ -135,14 +129,13 @@ void DeferredRenderer::BindGeometryPass()
 	};
 
 	// See Step 11
-	mDeviceContext->OMSetRenderTargets(BUFFER_COUNT, rtvs, mDepthStencilView);
+	mDeviceContext->OMSetRenderTargets(BUFFER_COUNT, rtvs, mDSV.Get());
 	// See ClearBuffer
 	mDeviceContext->ClearRenderTargetView(mGBuffer[0].rtv, Colors::LightSteelBlue);
 	mDeviceContext->ClearRenderTargetView(mGBuffer[1].rtv, Colors::Black);
 	mDeviceContext->ClearRenderTargetView(mGBuffer[2].rtv, Colors::Black);
-	mDeviceContext->ClearDepthStencilView(mDepthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0u);
+	//mDeviceContext->ClearDepthStencilView(mDSV.Get(), D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0u);
 
-	mDeviceContext->IASetInputLayout(mOpaqueVertexShader.GetInputLayout());
 	mDeviceContext->VSSetShader(mOpaqueVertexShader.Get(), nullptr, 0u);
 
 	mDeviceContext->RSSetViewports(1u, &mViewport);
@@ -155,18 +148,29 @@ void DeferredRenderer::BindGeometryPass()
 
 void DeferredRenderer::BindLightingPass()
 {
+	mDeviceContext->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	mDeviceContext->IASetInputLayout(mLightingVertexShader.GetInputLayout());
 
+	mDeviceContext->OMSetRenderTargets(1u, mRTV.GetAddressOf(), nullptr);
+
+	mDeviceContext->VSSetShader(mLightingVertexShader.Get(), nullptr, 0u);
+
+	mDeviceContext->RSSetViewports(1u, &mViewport);
+	mDeviceContext->RSSetState(mRasterizerState.Get());
+
+	mDeviceContext->OMSetDepthStencilState(mDepthStencilState.Get(), 0u);
+
+	mDeviceContext->PSSetShader(mLightingPixelShader.Get(), nullptr, 0u);
+	// Bind GBuffer resources
+	mDeviceContext->PSSetShaderResources(0u, 1u, &mGBuffer[0].srv);
+	mDeviceContext->PSSetShaderResources(1u, 1u, &mGBuffer[1].srv);
+	mDeviceContext->PSSetShaderResources(2u, 1u, &mGBuffer[2].srv);
+	
+	//mDeviceContext->PSSetSamplers(0u, 1u, mSamplerState.GetAddressOf());
+	mDeviceContext->PSSetSamplers(0u, 1u, mShadowSamplerState.GetAddressOf());
 }
 
 void DeferredRenderer::BindTransparentPass()
 {
 
-}
-
-void DeferredRenderer::Draw()
-{
-	/*for (auto renderObject : mRenderObjects)
-	{
-
-	}*/
 }
--- lab-1-intro/Shaders/DeferredLightingPS.hlsl
+++ lab-1-intro/Shaders/DeferredLightingPS.hlsl
@@ -1,24 +1,26 @@
-struct Gbuffer
-{
-    float4 DiffuseSpec : SV_Target0;
-    float3 Normal : SV_Target1;
-    float3 WorldPos : SV_Target2;
-};
+Texture2D<float4> unusedTexture : register(t0);
+
+Texture2D<float4> diffuseTexture : register(t1);
+Texture2D<float3> normalTexture : register(t2);
+Texture2D<float3> posTexture : register(t3);
+
+SamplerComparisonState shadowSamplerState : SAMPLER : register(s0);
 
 struct PS_IN
 {
-    
+    float4 inPosition : SV_POSITION;
+    float2 inTexCoord : TEXCOORD;
 };
 
-[earlydepthstencil]
-Gbuffer main(PS_IN input) : SV_Target
+float4 main(PS_IN input) : SV_Target
 {
-    Gbuffer output = (Gbuffer) 0;
+    float4 diffuseColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
+    diffuseTexture.Load(diffuseColor.xyz);
     
     //output.LightAccumulation = (ambient + emissive);
     //output.Diffuse = diffuse;
     //output.Specular = float4(specular.rgb, log2(specularPower) / 10.5f);
     //output.NormalVS = N;
  
-    return output;
+    return diffuseColor;
 }
