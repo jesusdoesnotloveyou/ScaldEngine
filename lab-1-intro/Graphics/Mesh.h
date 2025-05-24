856cd8b57da8b8b765df276afd72a5eef3985a0f
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-05-24 14:07:11 +0300

    indeces pass by value in ctor fixed

--- lab-1-intro/Graphics/Mesh.h
+++ lab-1-intro/Graphics/Mesh.h
@@ -12,7 +12,7 @@ class Mesh
 private:
 	Mesh(ID3D11DeviceContext* deviceContext);
 public:
-	Mesh(ID3D11Device* device, ID3D11DeviceContext* deviceContext, std::vector<VertexTex>& vertices, std::vector<DWORD> indeces);
+	Mesh(ID3D11Device* device, ID3D11DeviceContext* deviceContext, std::vector<VertexTex>& vertices, std::vector<DWORD>& indeces);
 	Mesh(const Mesh& mesh);
 
 	void Draw();
