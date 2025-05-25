4448b0c2ad4a822d66a84dcc9841df12127d9664
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-05-25 20:36:33 +0300

    Uniform light parameters structure for deferred shading has been added

--- lab-1-intro/Graphics/Light/LightHelper.h
+++ lab-1-intro/Graphics/Light/LightHelper.h
@@ -8,9 +8,9 @@ using namespace DirectX;
 enum class ELightType : uint8_t
 {
 	None = 0,
+	Directional,
 	Point,
 	Spot,
-	Directional,
 	MAX = 4
 };
 
@@ -57,4 +57,20 @@ struct SpotLightParams
 	XMFLOAT3 attenuation = { 0.0f, 0.0f, 0.0f };			// 12 bytes
 	float pad = 0.0f;										// 4 bytes
 															// 80 bytes
+};
+
+// Deferred Shading
+struct LIGHT_DESC
+{
+	LIGHT_DESC() { ZeroMemory(this, sizeof(this)); }
+
+	XMFLOAT4 ambient = { 0.0f, 0.0f, 0.0f, 0.0f };	// only directional
+	XMFLOAT4 diffuse = { 1.0f, 1.0f, 1.0f, 1.0f };
+	XMFLOAT4 specular = { 0.0f, 0.0f, 0.0f, 0.0f };
+	XMFLOAT3 direction = { 0.0f, 0.0f, 0.0f };		// directional and spot
+	float spot = 0.0f;								// only spot
+	XMFLOAT3 position = { 0.0f, 0.0f, 0.0f };		// spot and omni
+	ELightType lightType = ELightType::None;
+	XMFLOAT3 attenuation = { 0.0f, 0.0f, 0.0f };	// spot and omni
+	float range = 0.0f;								// spot and omni
 };
