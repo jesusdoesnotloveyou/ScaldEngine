9d11afda7bee9bb75b7d0def557b3a05cc2f519d
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-05-28 19:55:25 +0300

    light volume feature for rendering lights in deferred rendering

--- lab-1-intro/Graphics/Light/Light.cpp
+++ lab-1-intro/Graphics/Light/Light.cpp
@@ -1,4 +1,5 @@
 #include "Light.h"
+#include "../../Objects/Geometry/3D/Shapes.h"
 
 Light::Light(const std::string& filePath)
     :
@@ -9,6 +10,7 @@ Light::Light(const std::string& filePath)
 {
     LightParams = new LIGHT_DESC{};
 	modelPath = filePath;
+    mCollisionComponent->DisableCollision();
 }
 
 Light::~Light() noexcept
@@ -18,6 +20,21 @@ Light::~Light() noexcept
 
 void Light::Init(ID3D11Device* pDevice, ID3D11DeviceContext* pDeviceContext, const std::string& filePath, const std::wstring& texturePath)
 {
+    UpdateLightParams();
+    //if (LightType == ELightType::Directional)
+    //{
+    //    // CreateScreenQuad
+    //}
+    
+    if (LightType == ELightType::Point || LightType == ELightType::Spot)
+    {
+        std::vector<VertexTex> volumeVertices;
+        std::vector<DWORD> volumeIndeces;
+        Shapes::GetSphereShape(volumeVertices, volumeIndeces, 1.0f /*hard - coded value just for now*/, 8, 16);
+
+        LightVolume = new Mesh(pDevice, pDeviceContext, volumeVertices, volumeIndeces);
+    }
+
 	SceneGeometry::Init(pDevice, pDeviceContext, modelPath, texturePath);
 
     GenerateViewMatrix();
@@ -27,6 +44,7 @@ void Light::Init(ID3D11Device* pDevice, ID3D11DeviceContext* pDeviceContext, con
 void Light::Update(const ScaldTimer& st)
 {
 	SceneGeometry::Update(st);
+    UpdateLightParams();
     // if directional light is moving
     //GenerateViewMatrix();
     //GenerateOrthographicProjectionMatrix(100.0f, 100.0f, 0.1f, 100.0f);
@@ -37,13 +55,21 @@ void Light::Draw(const XMMATRIX& viewMatrix, const XMMATRIX& projectionMatrix)
 	SceneGeometry::Draw(viewMatrix, projectionMatrix);
 }
 
+void Light::DrawLightVolume(ID3D11DeviceContext* pDeviceContext)
+{
+    pDeviceContext->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    pDeviceContext->IASetVertexBuffers(0u, 1u, LightVolume->GetVertexBuffer().GetAddressOf(), LightVolume->GetVertexBuffer().GetStridePtr(), LightVolume->GetVertexBuffer().GetOffsetPtr());
+    pDeviceContext->IASetIndexBuffer(LightVolume->GetIndexBuffer().Get(), DXGI_FORMAT_R32_UINT, 0u);
+    pDeviceContext->DrawIndexed(LightVolume->GetIndexBuffer().GetBufferSize(), 0u, 0);
+}
+
 void Light::SetAmbientColor(float x, float y, float z, float w)
 {
     if (!LightParams) return;
     LightParams->ambient = XMFLOAT4(x, y, z, w);
 }
 
-XMFLOAT4 Light::GetAmbientColor()
+XMFLOAT4 Light::GetAmbientColor()const
 {
     return LightParams ? LightParams->ambient : XMFLOAT4{};
 }
@@ -54,7 +80,7 @@ void Light::SetDiffuseColor(float x, float y, float z, float w)
     LightParams->diffuse = XMFLOAT4(x, y, z, w);
 }
 
-XMFLOAT4 Light::GetDiffuseColor()
+XMFLOAT4 Light::GetDiffuseColor()const
 {
     return LightParams ? LightParams->diffuse : XMFLOAT4{};
 }
@@ -65,7 +91,7 @@ void Light::SetSpecularColor(float x, float y, float z, float w)
     LightParams->specular = XMFLOAT4(x, y, z, w);
 }
 
-XMFLOAT4 Light::GetSpecularColor()
+XMFLOAT4 Light::GetSpecularColor()const
 {
     return LightParams ? LightParams->specular : XMFLOAT4{};
 }
@@ -81,7 +107,7 @@ void Light::SetDirection(float x, float y, float z)
     XMStoreFloat3(&LightParams->direction, XMVector3Normalize(XMVectorSet(x, y, z, 0.0f)));
 }
 
-XMFLOAT3 Light::GetDirection()
+XMFLOAT3 Light::GetDirection()const
 {
     return LightParams ? LightParams->direction : XMFLOAT3{};
 }
@@ -92,7 +118,7 @@ void Light::SetAttenuation(float x, float y, float z)
     LightParams->attenuation = XMFLOAT3(x, y, z);
 }
 
-XMFLOAT3 Light::GetAttenuation()
+XMFLOAT3 Light::GetAttenuation()const
 {
     return LightParams ? LightParams->attenuation : XMFLOAT3{};
 }
