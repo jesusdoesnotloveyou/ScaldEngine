fd6e2912127b2bdde88f9e1c9eb4f56b3e94e54d
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-05-29 19:52:39 +0300

    shadows + point lights; IMP: pending refactor

--- lab-1-intro/Shaders/DeferredLightingPS.hlsl
+++ lab-1-intro/Shaders/DeferredLightingPS.hlsl
@@ -33,6 +33,7 @@ float3 GetShadowCoords(int layer, float3 worldPos)
 cbuffer ConstantBufferData : register(b1)
 {
     float4 gEyePos;
+    matrix gView;
 }
 
 struct UniLight
@@ -61,28 +62,27 @@ struct PS_IN
     float2 inTexCoord : TEXCOORD;
 };
 
-float3 CalcDirectionalLight(UniLight light, uniform float3 posW, uniform float3 normal, uniform float3 toEye)
+float3 CalcDirectionalLight(UniLight light, uniform float3 posW, uniform float3 normal, uniform float4 toEye)
 {
-    float3 lightDir = -light.direction;
+    float3 lightDir = normalize(-light.direction);
     float3 ambient = light.ambient.xyz * light.ambient.w;
     float4 diffuse = light.diffuse;
     float4 specular = light.specular;
     
     // necessary vectors
-    float3 viewDir = normalize(toEye - posW);
-    float3 lightVector = normalize(lightDir);
-    float3 reflectLight = normalize(reflect(-lightVector, normal));
+    float3 viewDir = normalize(toEye.xyz - posW);
+    float3 reflectLight = normalize(reflect(-lightDir, normal));
     
-    float3 diffuseLightIntensity = saturate(max(dot(lightVector, normal), 0.0f));
+    float3 diffuseLightIntensity = saturate(max(dot(lightDir, normal), 0.0f));
     float3 diffuseLight = diffuseLightIntensity * diffuse.xyz * diffuse.w;
     
     float3 specularIntensity = 0.5f * pow(max(dot(reflectLight, viewDir), 0.0f), 200.0f); // * specular.xyz
     float3 specularLight = saturate(specularIntensity);
     
-    return ambient * float3(0.6f, 0.6f, 0.6f) + diffuseLight + specularLight;
+    return diffuseLight + specularLight;
 }
 
-float3 CalcPointLight(UniLight light, uniform float3 posW, uniform float3 normal, uniform float3 toEye)
+float3 CalcPointLight(UniLight light, uniform float3 posW, uniform float3 normal, uniform float4 toEye)
 {    
     float4 diffuse = light.diffuse;
     float4 specular = light.specular;
@@ -90,9 +90,9 @@ float3 CalcPointLight(UniLight light, uniform float3 posW, uniform float3 normal
     float3 attenuation = light.attenuation;
     
     // necessary vectors
-    float3 V = normalize(toEye - posW);
-    float3 L = -normalize(lightPos - posW);
-    float3 R = normalize(reflect(L, normal));
+    float3 V = normalize(toEye.xyz - posW);
+    float3 L = normalize(lightPos - posW);
+    float3 R = normalize(reflect(-L, normal));
     
     float3 diffuseLightIntensity = saturate(max(dot(L, normal), 0.0f));
     float3 diffuseLight = diffuseLightIntensity * diffuse.xyz * diffuse.w;
@@ -101,15 +101,42 @@ float3 CalcPointLight(UniLight light, uniform float3 posW, uniform float3 normal
     float3 specularLight = saturate(specularIntensity);
     
     float distanceToLight = distance(lightPos, posW);
-    float attenuationFactor = pow(attenuation.x + attenuation.y * distanceToLight + attenuation.z * pow(distanceToLight, 2), -1);
+    if (distanceToLight > light.range)
+        return float3(0.0f, 0.0f, 0.0f);
     
+    float attenuationFactor = (1.0f - distanceToLight / light.range);
+    attenuationFactor *= attenuationFactor;
     return (diffuseLight + specularLight) * attenuationFactor;
 }
 
-float3 CalcSpotLight()
+void CalcSpotLight(UniLight L, float3 posW, float3 normal, float3 toEye, out float4 diffuse, out float4 spec)
 {
-    return float3(1.0f, 1.0f, 1.0f);
+    diffuse = float4(0.0f, 0.0f, 0.0f, 0.0f);
+    spec = float4(0.0f, 0.0f, 0.0f, 0.0f);
+    
+    float3 lightVec = L.position - posW;
+    float d = length(lightVec);
+    
+    if(d > L.range ) return;
+    
+    lightVec /= d;
+    
+    float diffuseFactor = dot(lightVec, normal);
 
+    [flatten]
+    if (diffuseFactor > 0.0f)
+    {
+        float3 v = reflect(-lightVec, normal);
+        float specFactor = pow(max(dot(v, toEye), 0.0f), 200.0f);
+        diffuse = diffuseFactor * L.diffuse;
+        spec = specFactor * L.specular;
+    }
+    
+    float spot = pow(max(dot(-lightVec, L.direction), 0.0f), L.spot);
+    float att = spot / dot(L.attenuation, float3(1.0f, d, d * d));
+    
+    diffuse *= att;
+    spec *= att;
 }
 
 float4 main(PS_IN input) : SV_Target
@@ -118,25 +145,58 @@ float4 main(PS_IN input) : SV_Target
     const int POINT = 2;
     const int SPOT = 3;
     
-    float3 appliedLight = float3(0.0f, 0.0f, 0.0f);
-    
     float4 diffuseColor = diffuseTexture.Load(input.inPosition.xyz);
     float3 normal = normalTexture.Load(input.inPosition.xyz);
     float3 pos = posTexture.Load(input.inPosition.xyz);
     
+    float3 appliedLight = float3(0.0f, 0.0f, 0.0f);
+    
+    int layer = 3;
+    float4 worldView = mul(float4(pos, 1.0f), gView);
+    
+    float viewDepth = abs(worldView.z);
+    for (int i = 0; i < 4; ++i)
+    {
+        if (viewDepth < CascData.Distances[i])
+        {
+            layer = i;
+            break;
+        }
+    }
+    
     uint width, height, elements, levels;
     shadowMaps.GetDimensions(0, width, height, elements, levels);
+    
+    const float dx = 1.0f / width;
+    const float2 offsets[9] =
+    {
+        float2(-dx, -dx), float2(0.0f, -dx), float2(dx, -dx),
+        float2(-dx, 0.0f), float2(0.0f, 0.0f), float2(dx, 0.0f),
+        float2(-dx, +dx), float2(0.0f, +dx), float2(dx, +dx)
+    };
+    
+    float3 shadowTexCoords = GetShadowCoords(layer, pos);
     
     [branch]
     if (Light.lightType == DIRECTIONAL)
     {
-        appliedLight += CalcDirectionalLight(Light, pos, normal, gEyePos.xyz);
+        float shadow = 0.0f;
+        if (saturate(shadowTexCoords.x) == shadowTexCoords.x && saturate(shadowTexCoords.y) == shadowTexCoords.y)
+        {
+            float currentDepth = shadowTexCoords.z - 0.001f;
+    
+            [unroll]
+            for (int i = 0; i < 9; ++i)
+            {
+                shadow += SampleShadowMap(layer, shadowTexCoords.xy + offsets[i], currentDepth);
+            }
+            shadow = shadow / 9.0f;
+        }
+        appliedLight += CalcDirectionalLight(Light, pos, normal, gEyePos) * shadow;
     }
     else
     {
-        appliedLight += CalcPointLight(Light, pos, normal, gEyePos.xyz);
+        appliedLight += CalcPointLight(Light, pos, normal, gEyePos);
     }
     return diffuseColor * float4(appliedLight, 1.0f);
-    
-    //output.LightAccumulation = (ambient + emissive);
 }
