fcde51c5151609b2501596219d0dc3d09864249e
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-06-02 20:26:48 +0300

    still nothing

--- lab-1-intro/Graphics/Particles/ParticleSystem.h
+++ lab-1-intro/Graphics/Particles/ParticleSystem.h
@@ -1,6 +1,13 @@
 #pragma once
 
+#include "../../ScaldException.h"
 #include "../ScaldCoreTypes.h"
+#include "../Shaders.h"
+
+#include <d3d11.h>
+#include <wrl.h>
+
+#include <vector>
 
 struct Particle
 {
@@ -18,10 +25,18 @@ struct Particle
     float colorDelta[4];    // how the color changes with time
 };
 
+struct SortConstantBuffer
+{
+    UINT g_iLevel;
+    UINT g_iLevelMask;
+    UINT g_iWidth;
+    UINT g_iHeight;
+};
+
 class ParticleSystem 
 {
 public:
-    ParticleSystem(int maxParticles, XMVECTOR origin);
+    ParticleSystem(ID3D11Device* device, ID3D11DeviceContext* deviceContext, int maxParticles, XMVECTOR origin);
     virtual void Update(float elapsedTime) = 0;
     virtual void Render() = 0;
     virtual int Emit(int numParticles);
@@ -29,12 +44,93 @@ public:
     virtual ~ParticleSystem() noexcept;
 
 protected:
+
+    template<typename T>
+    HRESULT CreateRWStructuredBuffer(ID3D11Device* device, ID3D11Buffer** buffer, const std::vector<T>& bufferData)
+    {
+        UINT stride = (UINT)sizeof(T);
+        UINT byteWidth = stride * (UINT)bufferData.size();
+
+        D3D11_BUFFER_DESC desc;
+        desc.ByteWidth = byteWidth;
+        desc.Usage = D3D11_USAGE_DEFAULT; // without CPU write
+        desc.BindFlags = D3D11_BIND_UNORDERED_ACCESS | D3D11_BIND_SHADER_RESOURCE;
+        desc.CPUAccessFlags = 0;
+        desc.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
+        desc.StructureByteStride = stride;
+
+        if (bufferData.data())
+        {
+            D3D11_SUBRESOURCE_DATA data;
+            data.pSysMem = bufferData.data();
+            data.SysMemPitch = 0;
+            data.SysMemSlicePitch = 0;
+            return device->CreateBuffer(&desc, &data, buffer);
+        }
+
+        return device->CreateBuffer(&desc, nullptr, buffer);
+    }
+
+    template<typename T>
+    HRESULT CreateRWStructuredBufferCPUWrite(ID3D11Device* device, ID3D11Buffer** buffer, const std::vector<T>& bufferData)
+    {
+        UINT stride = (UINT)sizeof(T);
+        UINT byteWidth = stride * (UINT)bufferData.size();
+
+        D3D11_BUFFER_DESC desc;
+        desc.ByteWidth = byteWidth;
+        desc.Usage = D3D11_USAGE_DYNAMIC; // without CPU write
+        desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+        desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+        desc.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
+        desc.StructureByteStride = stride;
+
+        if (bufferData.data())
+        {
+            D3D11_SUBRESOURCE_DATA data;
+            data.pSysMem = bufferData.data();
+            data.SysMemPitch = 0;
+            data.SysMemSlicePitch = 0;
+            return device->CreateBuffer(&desc, &data, buffer);
+        }
+
+        return device->CreateBuffer(&desc, nullptr, buffer);
+    }
+
     virtual void InitializeParticle(int index) = 0;
 
-    Particle* particleList;     // particles for this emitter
+    // Graphics context. Graphics object manages these resources.
+    ID3D11Device* mDevice = nullptr;
+    ID3D11DeviceContext* mDeviceContext = nullptr;
+
+    std::vector<Particle> particleList;     // particles for this emitter
     int       maxParticles;     // maximum number of particles in total
     int       numParticles;     // current number of particles
     XMVECTOR  origin;           // center of the particle system
     float     accumulatedTime;  // track when was last particle emitted
     XMVECTOR  force;            // force (gravity, wind, etc.) acting on the system
+
+    ID3D11Buffer* particelBuffer = nullptr;
+    Microsoft::WRL::ComPtr<ID3D11UnorderedAccessView> particleBufferUAV;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> particleBufferSRV;
+
+    ID3D11Buffer* sortListBuffer = nullptr;
+    Microsoft::WRL::ComPtr<ID3D11UnorderedAccessView> sortListBufferUAV;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> sortListBufferSRV;
+
+    ID3D11Buffer* sortListBuffer2 = nullptr;
+    Microsoft::WRL::ComPtr<ID3D11UnorderedAccessView> sortListBufferUAV2;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> sortListBufferSRV2;
+    
+    ID3D11Buffer* deadListBuffer = nullptr;
+    Microsoft::WRL::ComPtr<ID3D11UnorderedAccessView> deadListBufferUAV;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> deadListBufferSRV;
+    
+    ID3D11Buffer* indirectArgsBuffer = nullptr;
+    ID3D11Buffer* constSortBuf = nullptr;
+     
+    ComputeShader bitonicSortShader;
+    ComputeShader bitonicTransposeShader;
+
+    SortConstantBuffer mSortData;
 };
