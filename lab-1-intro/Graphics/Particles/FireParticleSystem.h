e673edc16de16f8dc403b4c37703e99cb87cef82
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-06-04 16:31:47 +0300

    full particle setup: UAVs, SRVs, buffers, sort, system initialization, render, emit, simulation

--- lab-1-intro/Graphics/Particles/FireParticleSystem.h
+++ lab-1-intro/Graphics/Particles/FireParticleSystem.h
@@ -5,27 +5,20 @@
 class FireParticleSystem final : ParticleSystem
 {
 public:
-    FireParticleSystem(ID3D11Device* device, ID3D11DeviceContext* deviceContext, int maxParticles, XMVECTOR origin);
+    FireParticleSystem(ID3D11Device* device, ID3D11DeviceContext* deviceContext, int maxParticles, XMVECTOR origin, ThirdPersonCamera* camera);
     virtual void Update(float elapsedTime) override;
+    virtual void Simulate(float elapsedTime) override;
     virtual void Render() override;
-    virtual int Emit(int numParticles) override;
+    virtual void Emit(int numParticles) override;
     virtual void InitializeSystem() override;
-    void InitializeParticles(int count);
     virtual ~FireParticleSystem() noexcept override;
+
 protected:
     virtual void InitializeParticle(int index) override;
 
 private:
-    void Simulate(float elapsedTime);
-    void SortParticles();
-
-private:
     VertexShader mFireVertexShader;
-    GeometryShader mFireGeometryShader;
     PixelShader mFirePixelShader;
     ComputeShader mFireSimulateShader;
     ComputeShader mFireEmitShader;
-
-    Microsoft::WRL::ComPtr<ID3D11SamplerState> mParticleSamplerClamp;
-    ID3D11ShaderResourceView* mBillboardTexture = nullptr;
 };
