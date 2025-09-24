1241f9cb354949a31fb9bbec7113bd54d3a3f8b0
jesusdoesnotloveyou <artyomkozlovsky@gmail.com>
2025-09-24 10:32:45 +0300

    DX helper file with dx11 stuff

---
+++ lab-1-intro/Graphics/DXHelper.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <wrl.h>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <d3d.h>
+
+#include "ScaldCoreTypes.h"
+
+#include <memory>
+#include <vector>
+#include <string>
