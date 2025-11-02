34b5d88fbb025f23d8227f8bb2c5db2211ed06df
jesusdoesnotloveyou <artyomkozlovsky@gmail.com>
2025-11-02 17:47:26 +0300

    helper class with d3d stuff

---
+++ ScaldEngine/Graphics/DXHelper.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <wrl.h>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <d3d.h>
+
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "dxgi.lib")
+
+#include <vector>
+#include <memory>
